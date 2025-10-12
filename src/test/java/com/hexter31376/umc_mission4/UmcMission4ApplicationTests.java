2098bd41fe6df76df5b22da30559bcee17a83c7c
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-13 01:22:04 +0900

    initial commit

---
+++ src/test/java/com/hexter31376/umc_mission4/UmcMission4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexter31376.umc_mission4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcMission4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
